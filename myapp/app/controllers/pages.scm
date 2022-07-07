3f72b2b9797c3ceb2b35eacf7c7356f5972f5310
mbcladwell <mbcladwell@gmail.com>
2022-07-07 15:56:56 -0400

    changes

--- myapp/app/controllers/pages.scm
+++ myapp/app/controllers/pages.scm
@@ -3,7 +3,7 @@
 ;; This file is generated automatically by GNU Artanis.
 (define-module (myapp app controllers pages)
   #:use-module (artanis mvc controller)
-  #:use-module (myapp lib mylib))
+  #:use-module (lib mylib))
 
 (define-artanis-controller pages) ; DO NOT REMOVE THIS LINE!!!
 
